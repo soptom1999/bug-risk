3884f465359758b974f7ccee0d3d7dbb8301f5da
RaBa <sumyashahrin20@gmail.com>
2025-11-13 23:01:47 +0100

    loaded entire dataset

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import numpy as np
+
+# ===============================================
+# 1. LOAD THE ENTIRE DATASET 
+# ===============================================
+df = pd.read_csv(r"E:\Software engineering projec\bug-risk\data\commits_small.csv")
